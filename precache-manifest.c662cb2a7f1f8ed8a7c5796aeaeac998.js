d834e0f945bab04fc36d8ef0fb3977e7eb0e5579
KaishAli <360mohdkaishali.cs@gmail.com>
2022-02-15 01:30:24 +0530

    Updates

---
+++ precache-manifest.c662cb2a7f1f8ed8a7c5796aeaeac998.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbf9310aa801b5b2cd89",
+    "url": "/react-todo-list/static/css/main.9c4096b8.chunk.css"
+  },
+  {
+    "revision": "bbf9310aa801b5b2cd89",
+    "url": "/react-todo-list/static/js/main.bbf9310a.chunk.js"
+  },
+  {
+    "revision": "213912c3d00d5092cf6f",
+    "url": "/react-todo-list/static/css/1.69a4ab3c.chunk.css"
+  },
+  {
+    "revision": "213912c3d00d5092cf6f",
+    "url": "/react-todo-list/static/js/1.213912c3.chunk.js"
+  },
+  {
+    "revision": "9061ff665ca8ca5bb17c",
+    "url": "/react-todo-list/static/js/runtime~main.9061ff66.js"
+  },
+  {
+    "revision": "4eb253c07061c934764504465102e40e",
+    "url": "/react-todo-list/static/media/index.4eb253c0.mjs"
+  },
+  {
+    "revision": "f79a78603c804b165a07d6985bf2f253",
+    "url": "/react-todo-list/index.html"
+  }
+];
